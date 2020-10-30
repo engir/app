6971a9d2839d35e2ebd68a9610004e52d5ee1278
Артём <63098004+engir@users.noreply.github.com>
2020-10-30 15:39:40 +0300

    Updates

---
+++ precache-manifest.73e9ddd9fed803060d7da18143305f5b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717640bd6671a39c12cc2fe6de257c65",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f1aac36059817210c06",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "7f1e6d215390786d644d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4f1aac36059817210c06",
+    "url": "./static/js/2.c5dd5ffe.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.c5dd5ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1e6d215390786d644d",
+    "url": "./static/js/main.426c4b8e.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
