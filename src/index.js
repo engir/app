3d323800cd5f4954bf3941d00c5ad529800abeae
Артём <63098004+engir@users.noreply.github.com>
2021-01-02 21:39:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
+import mysql from 'mysql2';
 import App from './App';
 import registerServiceWorker from './sw';
+
+
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "usersdb",
+  password: "пароль_от_сервера"
+});
+connection.connect();
 
 // Init VK App
 connect.send('VKWebAppInit', {});
