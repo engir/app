3b99ff803b29c449c90c8de02793d37dbf6864b6
Артём <63098004+engir@users.noreply.github.com>
2021-01-03 14:25:52 +0300

    Updates

---
+++ precache-manifest.1774ec60a58e9a82ba4ce25c8151cd69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db09ce08d08f7144f6899a6ddec06c1b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06a226cee8aa7d58e38c",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "06a226cee8aa7d58e38c",
+    "url": "./static/js/2.9c2661d5.chunk.js"
+  },
+  {
+    "revision": "eadf68d2e907bf3798213df7db41d5eb",
+    "url": "./static/js/2.9c2661d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329548f72099494173c9",
+    "url": "./static/js/main.c9469aa0.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  }
+]);
