3b3e4b70d7b676c26d16fe6edfc3b35f2c574c4d
Артём <63098004+engir@users.noreply.github.com>
2021-01-06 16:20:49 +0300

    Updates

--- precache-manifest.7147e27e6d8b5a82eab8ca356243009c.js
+++ precache-manifest.7147e27e6d8b5a82eab8ca356243009c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "64c81ccc669e36a0d019ea3305fc7813",
+    "revision": "1f08d88c3a407b8d589df1a14a84fd4d",
     "url": "./index.html"
   },
   {
-    "revision": "b87e09b2c28b2f897c31",
-    "url": "./static/js/2.1ac12b78.chunk.js"
+    "revision": "e5daf4b2b9a744b50189",
+    "url": "./static/js/2.94cfdb52.chunk.js"
   },
   {
     "revision": "98c45f5e477a42a2903d2fe6c7d523c7",
-    "url": "./static/js/2.1ac12b78.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.94cfdb52.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3f82caa7cdf0f9d407ca",
-    "url": "./static/js/main.a6f4c637.chunk.js"
+    "revision": "16847d22dadfdf5a0980",
+    "url": "./static/js/main.0325fd67.chunk.js"
   },
   {
     "revision": "e86c2ff2f1eb78554e57",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c797a26a3292907cdba426d9e4283432.js"
+  "./precache-manifest.7147e27e6d8b5a82eab8ca356243009c.js"
 );
 
 self.addEventListener('message', (event) => {
