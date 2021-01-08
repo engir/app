1a31a50468281e9440affbea022c56f9d859ff00
Артём <63098004+engir@users.noreply.github.com>
2021-01-08 18:02:16 +0300

    Global Update

--- src/index.js
+++ src/index.js
@@ -3,16 +3,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
-import App from './App';
-import mysql2 from 'mysql2';
-import db from './db'
+import bridge from '@vkontakte/vk-bridge';
+import axios from 'axios';
 import registerServiceWorker from './sw';
+import { verifyLaunchParams } from './launchParams';
+
+function getValue(array, keyName) {
+  return array.filter(el => el.key === keyName)[0].value
+}
+
+const launchParams = window.location.search.slice(1);
+const userStatus = verifyLaunchParams(launchParams, "6O1XitpFwXIGtHaxvdD3");
+if (userStatus != 0)  {
+
+  if (getValue(userStatus, "vk_is_app_user") == 1) {
+    ReactDOM.render(
+      <h1> Вы являетесь пользователем приложения. Ожидайте аутентификации. </h1>,
+      document.getElementById('root')
+    );
+
+    const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://AppUser:XbB1620@cluster0.7srov.mongodb.net/app?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+  } else {
+    alert("Регистрация недоступна.")
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
 
 // Init VK App
 connect.send('VKWebAppInit', {});
 
-db();
 // Service Worker For Cache
-registerServiceWorker();
-
-ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
---
+++ src/validateUser.js
@@ -0,0 +1,7 @@
+function $_GET(key) {
+    var p = window.location.search;
+    p = p.match(new RegExp(key + '=([^&=]+)'));
+    return p ? p[1] : false;
+}
+
+console.log($_GET('id'));
